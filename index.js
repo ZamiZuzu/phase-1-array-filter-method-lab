c6bdde36da01875a1c48c88e97fce00d03fbdc7e
Christopher Valle <chrisbv@live.com>
2021-10-15 09:31:08 -0500

    commit tree ad79bbe3bf1f33a7914409ebed5f048032d7a69d

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution here
+// function findMatching(collection, name){
+//     let list = [];
+//     for (let i = 0; i < collection.length; i++){
+//         if (collection[i] == name) {
+//             list.push(name);
+            
+//         }
+//     }return(list);
+// }
+
+function findMatching(collection, name){
+    return collection.filter( filtering =>
+        filtering.toLowerCase() === name.toLowerCase()
+    )
+}
+
+function fuzzyMatch(source, testString) {
+    return source.filter( possibleMatch =>
+      possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+    )
+  }
+
+  function matchName(collection, driver){
+    return collection.filter( filtering =>
+        filtering.name === driver
+    )
+}
